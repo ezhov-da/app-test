589ec5cb68bffb9f11ef2a7b38559f985b0806ad
Denis <ezhdenis@yandex.ru>
2018-01-09 16:58:36 +0300

    Update App.java

--- src/main/java/ru/ezhov/test/xml/App.java
+++ src/main/java/ru/ezhov/test/xml/App.java
@@ -1,25 +1,34 @@
 package ru.ezhov.xmlJaxb;
 
-import javax.xml.bind.JAXB;
-import java.io.IOException;
-import java.io.StringWriter;
+import java.io.InputStream;
 
 public class App {
     public static void main(String[] args) {
-        Directory directory = new Directory("test", null);
-        directory.addFile(new File("testFile", directory.getName()));
-        directory.addDirectory(new Directory("test dir", directory.getName()));
+//        Directory directory = new Directory("test", null);
+//        directory.addFile(new File("testFile", directory.getName()));
+//        directory.addDirectory(new Directory("test dir", directory.getName()));
+//
+//        StringWriter stringWriter = new StringWriter();
+//        JAXB.marshal(directory, stringWriter);
+//        String s = stringWriter.toString();
+//        try {
+//            stringWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        System.out.println(s);
 
-        StringWriter stringWriter = new StringWriter();
-        JAXB.marshal(directory, stringWriter);
-        String s = stringWriter.toString();
-        try {
-            stringWriter.close();
-        } catch (IOException e) {
+        try (InputStream inputStream =
+                     App.class.getResourceAsStream("/server/unpack.xml")) {
+
+            UnpackJarResources unpackJarResources =
+                    new UnpackJarResources(inputStream, ".");
+
+            unpackJarResources.createAndCopy();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.println(s);
 
     }
 }
