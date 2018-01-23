2f611e6712bdc4b98072b38df16e7a28053d41c8
Denis <ezhdenis@yandex.ru>
2018-01-23 11:04:31 +0300

    Create AppSpark.java

---
+++ src/main/java/ru/ezhov/test/websockets/AppSpark.java
@@ -0,0 +1,13 @@
+import java.util.logging.Logger;
+
+import static spark.Spark.*;
+
+public class AppSpark {
+    private static final Logger LOG = Logger.getLogger(AppSpark.class.getName());
+
+    public static void main(String[] args) {
+        port(4567);
+        webSocket("/test", EchoWebSocket.class);
+        get("/", (request, response) -> "test");
+    }
+}
