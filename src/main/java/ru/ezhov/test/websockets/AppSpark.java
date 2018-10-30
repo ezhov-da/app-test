72186832926e304030fce7c65f53587dbd02dba3
ezhov-da <ezhdenis@yandex.ru>
2018-10-30 21:09:05 +0300

    Play

--- src/main/java/ru/ezhov/test/websockets/AppSpark.java
+++ src/main/java/ru/ezhov/test/websockets/AppSpark.java
@@ -2,14 +2,21 @@ package ru.ezhov.test.websockets;
 
 import java.util.logging.Logger;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class AppSpark {
     private static final Logger LOG = Logger.getLogger(AppSpark.class.getName());
 
     public static void main(String[] args) {
         port(4567);
-        webSocket("/test", EchoWebSocket.class);
-        get("/", (request, response) -> "test");
+//        webSocket("/test", EchoWebSocket.class);
+        get("/", (request, response) -> {
+            request.headers().forEach(System.out::println);
+            request.attributes().forEach(System.out::println);
+            request.queryParams().forEach(System.out::println);
+            System.out.println(request);
+            return "test";
+        });
     }
 }
