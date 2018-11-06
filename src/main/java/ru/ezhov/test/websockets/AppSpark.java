668e2a0eff1a93d8dfb8e4547935a17abdba1a4c
ezhov-da <ezhdenis@yandex.ru>
2018-11-07 00:08:02 +0300

    Play

--- src/main/java/ru/ezhov/test/websockets/AppSpark.java
+++ src/main/java/ru/ezhov/test/websockets/AppSpark.java
@@ -1,22 +1,33 @@
 package ru.ezhov.test.websockets;
 
+import java.util.Scanner;
 import java.util.logging.Logger;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
+import static spark.Spark.*;
 
 public class AppSpark {
     private static final Logger LOG = Logger.getLogger(AppSpark.class.getName());
 
     public static void main(String[] args) {
         port(4567);
+        staticFileLocation("html");
+
 //        webSocket("/test", EchoWebSocket.class);
-        get("/", (request, response) -> {
-            request.headers().forEach(System.out::println);
-            request.attributes().forEach(System.out::println);
-            request.queryParams().forEach(System.out::println);
-            System.out.println(request);
-            return "test";
+//        get("/", (request, response) -> {
+//            request.headers().forEach(System.out::println);
+//            request.attributes().forEach(System.out::println);
+//            request.queryParams().forEach(System.out::println);
+//            System.out.println(request);
+//            return "test";
+//        });
+
+        post("file-up", (request, response) -> {
+            try (Scanner scanner = new Scanner(request.raw().getInputStream())) {
+                while (scanner.hasNextLine()) {
+                    System.out.println(scanner.nextLine());
+                }
+            }
+            return "qwqwqwqw";
         });
     }
 }
