d01bd3e96f5f630fbea01a8bbfd363b6ac991d37
ezhov-da <ezhdenis@yandex.ru>
2019-05-18 00:07:07 +0300

    Тестирование NDC и MDC LOG4J.

---
+++ src/main/java/ru/ezhov/test/log4j/MDCTest.java
@@ -0,0 +1,36 @@
+package ru.ezhov.test.log4j;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
+import org.apache.log4j.NDC;
+
+//%X
+public class MDCTest {
+    private static final Logger LOG = Logger.getLogger(NDCTest.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new MDCTestThread(i).start();
+        }
+    }
+}
+
+
+class MDCTestThread extends Thread {
+    private static final Logger LOG = Logger.getLogger(NDCTestThread.class);
+    private int number;
+
+    public MDCTestThread(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        MDC.put("Thread", number);
+        MDC.put("Test", "OK");
+        for (int i = 0; i < 3; i++) {
+            LOG.info(i);
+        }
+        MDC.clear();
+    }
+}
---
+++ src/main/java/ru/ezhov/test/log4j/NDCTest.java
@@ -0,0 +1,36 @@
+package ru.ezhov.test.log4j;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.NDC;
+
+//%x
+public class NDCTest {
+    private static final Logger LOG = Logger.getLogger(NDCTest.class);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            new NDCTestThread(i).start();
+        }
+    }
+}
+
+
+class NDCTestThread extends Thread {
+    private static final Logger LOG = Logger.getLogger(NDCTestThread.class);
+    private int number;
+
+    public NDCTestThread(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        NDC.push("Thread " + number);
+        for (int i = 0; i < 3; i++) {
+            LOG.info(i);
+        }
+        NDC.pop();
+        NDC.remove();
+    }
+}
