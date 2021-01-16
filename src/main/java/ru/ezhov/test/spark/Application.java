b89a1fd92db3ed4bf3490c46773055c29ef28502
ezhov-da <ezhdenis@yandex.ru>
2021-01-16 14:08:30 +0300

    feat: Spark file example

--- src/main/java/ru/ezhov/test/spark/Application.java
+++ src/main/java/ru/ezhov/test/spark/Application.java
@@ -1,11 +1,14 @@
 package ru.ezhov.test.spark;
 
-import static spark.Spark.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static spark.Spark.get;
 
 //port 4567
 public class Application {
     public static void main(String[] args) {
-        get("/:guid", (request, response) -> {
+        get("/guid/:guid", (request, response) -> {
             Thread.sleep(2000);
 
             request.headers().forEach(System.out::println);
@@ -13,5 +16,21 @@ public class Application {
 
             return "Hello World! " + request.params(":guid");
         });
+
+        get("/file", ((request, response) -> {
+            response.header("Content-Type", "image/jpeg");
+            response.header("Content-disposition", "attachment; filename=field.jpg;");
+
+            try (
+                    InputStream inputStream = Application.class.getResourceAsStream("/field.jpg");
+                    OutputStream outputStream = response.raw().getOutputStream()
+            ) {
+                byte[] bytes = new byte[inputStream.available()];
+                inputStream.read(bytes);
+                outputStream.write(bytes);
+                outputStream.flush();
+                return response;
+            }
+        }));
     }
 }
